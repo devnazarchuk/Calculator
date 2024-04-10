ad0a5ab503c01a444a35ab0d1028cbe74e6a4c79
Ar4uk69 <126593592+ar4uk69@users.noreply.github.com>
2024-04-10 22:25:05 +0200

    just trying write code

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-var navLinks = document.getElementById("navLinks");
-var faBars = document.getElementById("faBars");
-var faTimes = document.getElementById("faTimes");
+let navLinks = document.getElementById("navLinks");
+let faBars = document.getElementById("faBars");
+let faTimes = document.getElementById("faTimes");
 function showMenu() {
   navLinks.style.right = "0";
   faBars.style.display = "none";
@@ -11,3 +11,21 @@ function hideMenu() {
   faTimes.style.display = "none";
   faBars.style.display = "block";
 }
+/*--------------Me learn code--------------------*/
+let button_img = document.getElementById("button_img");
+
+button_img.onclick = function imageChange(){
+  let header_img = document.getElementById("header_img");
+  let mySrc = header_img.getAttribute("src");
+  if (mySrc === "/img/banner.png") {
+    header_img.setAttribute("src", "/img/banner2.png");
+  } else {
+    header_img.setAttribute("src", "/img/banner.png");
+  }
+};
+// let userName = document.getElementById("userName");
+// userName.onclick = function setUserName() {
+//   let myName = prompt("Please enter your name.");
+//   localStorage.setItem("name", myName);
+//   myHeading.textContent = "Edge is cool, " + myName;
+// }
