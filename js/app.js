dd277edf72fd1e4573d4d4fc4dd66321ca9965bd
Ar4uk69 <126593592+ar4uk69@users.noreply.github.com>
2024-04-13 17:41:34 +0200

    100% calculator

--- js/app.js
+++ js/app.js
@@ -13,3 +13,19 @@ function hideMenu() {
   faBars.style.display = "block";
 }
 /*--------------Me learn code--------------------*/
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+  display.value +=input;
+}
+function clearDisplay(){
+  display.value="";
+}
+function calculate(){
+  try{
+  display.value = eval(display.value);
+  }
+  catch(error){
+    display.value="Error"
+  }
+}
