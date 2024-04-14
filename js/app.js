6dd3581d1b2425d0b302e129b82314b47e413647
Ar4uk69 <126593592+ar4uk69@users.noreply.github.com>
2024-04-14 13:38:10 +0200

    Last commit
    100% only calculator

--- js/app.js
+++ js/app.js
@@ -1,19 +1,4 @@
 "use strict";
-let navLinks = document.getElementById("navLinks");
-let faBars = document.getElementById("faBars");
-let faTimes = document.getElementById("faTimes");
-function showMenu() {
-  navLinks.style.right = "0";
-  faBars.style.display = "none";
-  faTimes.style.display = "block";
-}
-function hideMenu() {
-  navLinks.style.right = "-40vw";
-  faTimes.style.display = "none";
-  faBars.style.display = "block";
-}
-/*--------------Me learn code--------------------*/
-
 // -----------------calculator-----------------//
 const display = document.getElementById("display");
 
@@ -30,11 +15,3 @@ function calculate() {
     display.value = "Error";
   }
 }
-//--------------------study---------------------------
-let username;
-document.getElementById("mySubmit").onclick = function () {
-  username = document.getElementById("myText").value;
-  document.getElementById("myH1").textContent = `Hello ${username}`;
-  document.getElementById("title").textContent = username + "'s site";
-};
-
